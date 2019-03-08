de82bce36bdad6eaca53f2cecdf5508cabcd63cb
Jude M Battista <jbattista20@my.whitworth.edu>
2019-03-07 16:06:43 -0800

    Updated parent utility file

--- utilities.py
+++ utilities.py
@@ -21,3 +21,25 @@ def writeListToFileOnNewlines(outfile, data):
 def readIntListFromFile(infile):
     return list(map(lambda x: int(x), infile.readline().strip().split(' ')))
 
+# compute the Hamming distance between two strings (or lists. Lists are better.)
+def hammingDistance(text0, text1):
+    return sum([ntide0 != ntide1 for ntide0, ntide1 in zip(text0, text1)])
+
+def genCousinsOfD(kmer, alphabet, distance):
+    # Generate all the possible locations for replacement
+    for positions in combinations(range(len(kmer)), distance):
+        #generate a every possible set of values to fill in each set of locations
+        # ... kind of. We will not include the last letter in the alphabet
+        for replacements in product(range(len(alphabet) - 1), repeat=distance):
+            # work with a list rather than a string for efficiency
+            cousin = list(kmer)
+            for pos, rep in zip(positions, replacements):
+                # if the letter is the same as the replacement, replace it with the 
+                # 'spare' letter from the alphabet
+                if cousin[pos] == alphabet[rep]:
+                    cousin[pos] = alphabet[-1]
+                else:
+                    cousin[pos] = alphabet[rep]
+            yield ''.join(cousin)
+
+
